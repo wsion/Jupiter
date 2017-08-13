299e000dd8d5040c0ebcaa152e195c40859fb0d8
wsion <waychanmail@gmail.com>
2017-08-13 19:36:04 +0800

    Unwanted project removed.

--- Jupiter/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Jupiter.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Jupiter/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jupiter.WebApiApplication" Language="C#" %>
