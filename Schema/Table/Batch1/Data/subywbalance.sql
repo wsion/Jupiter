fae58cb08ed9bc888912cda39c27f72a02bbbf5d
wsion <waychanmail@gmail.com>
2017-08-20 02:01:44 +0800

    Batch1 settings initialized.

--- Schema/Table/Batch1/Data/subywbalance.sql
+++ Schema/Table/Batch1/Data/subywbalance.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[subywbalance] --库存表
 (
 	[SUBBH]	NVARCHAR(20) NOT NULL, --门店编号
-	[HH]	NVARCHAR(20) NOT NULL PRIMARY KEY ([TJBH],[HH]), --商品编码
+	[HH]	NVARCHAR(20) NOT NULL PRIMARY KEY ([SUBBH],[HH]), --商品编码
 	[SJSL]	NVARCHAR(50) NULL, --库存数量
 	[LSJ]	NVARCHAR(50) NULL, --门店零售价
 	[LSJ1]	NVARCHAR(50) NULL, --门店会员价
